9116821ca809eb7d655c617ad930f81adb5ec5f3
Duke Nukem <garrettduke87@gmail.com>
2021-01-19 17:16:21 -0700

    slight adjustment to server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 const helpers = require('./utils/helpers');
 
@@ -21,7 +21,7 @@ const sess = {
     })
 };
 
-const hbs = exphbs.create({ helpers });
+// const hbs = exphbs.create({ helpers });
 const app = express();
 const PORT = process.env.PORT || 3001;
 
