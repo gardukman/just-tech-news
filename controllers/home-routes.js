42a8d38a2b3ce1b7a7a6bc06db66b3d4ebadfaeb
Duke Nukem <garrettduke87@gmail.com>
2021-01-21 15:45:57 -0700

    prior to heroku push

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,28 +4,29 @@ const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
     Post.findAll({
-        attributes: [
-            'id',
-            'title',
-            'content',
-            'created_at',
-        ],
-        include: [
-            {
-                model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
+        // attributes: [
+        //     "id",
+        //     "title",
+        //     "content",
+        //     "created_at",
+        // ],
+        // include: [
+        //     {
+        //         model: Comment,
+        //         attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        //         include: {
+        //             model: User,
+        //             attributes: ['username']
+        //         }
+        //     },
+        //     {
+        //         model: User,
+        //         attributes: ['username']
+        //     }
+        // ]
     })
         .then(dbPostData => {
+            console.log(dbPostData)
             // pass a single post object into the homepage template
             const posts = dbPostData.map(post => post.get({ plain: true }));
             res.render('homepage', { posts, loggedIn: req.session.loggedIn });
